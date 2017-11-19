6c2e0ea71c05c5de70449a68fea46e1e94b335c5
sakmor <sakmor@gmail.com>
2017-11-19 10:51:13 +0800

    修正跳躍時按住前進後退不會下墜的問題

---
+++ Assets/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.name == "Head")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 0.01f;
+    public float jump = 60;
+    private Rigidbody Rigidbody;
+    void Start()
+    {
+        Debug.Log("Hello");
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            float LastY = Rigidbody.velocity.y; //先把目前Y軸速度紀錄下來 (下墜值)
+            Rigidbody.velocity = gameObject.transform.forward * speed; //覆蓋速度，使Player往前
+            Rigidbody.velocity = new Vector3(Rigidbody.velocity.x, LastY, Rigidbody.velocity.z);//還原下墜值，使Player下墜
+        }
+        if (Input.GetKey("s"))
+        {
+            float LastY = Rigidbody.velocity.y; //先把目前Y軸速度紀錄下來 (下墜值)
+            Rigidbody.velocity = gameObject.transform.forward * -speed; //覆蓋速度，使Player往前
+            Rigidbody.velocity = new Vector3(Rigidbody.velocity.x, LastY, Rigidbody.velocity.z);//還原下墜值，使Player下墜
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, jump, 0));
+        }
+    }
+}
